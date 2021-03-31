69db030895e2338476a9bba467402d505382ec8e
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2021-03-31 23:00:43 +0200

    Update of the get_docker_info function : - change the exit code - check the argument at the beginning - manage differently the last column
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- src/util/get_docker_info.sh
+++ src/util/get_docker_info.sh
@@ -17,7 +17,7 @@ find_word_position() {
 	local WORD="$1"
 	shift
 	local STRING="$*"
-	STRING_SIZE=$(echo "$STRING" | wc -c)
+	STRING_SIZE=${#STRING}
 
 	for ((i=1; i<=STRING_SIZE; i++))
 	do
@@ -30,7 +30,13 @@ find_word_position() {
 	done
 }
 
-# Step 1 : Check if the attribute is in the docker columns list and find its next column
+# Step 1 : Check if the arguments are not empty
+if [ "$ATTRIBUTE" == "" ] || [ "$CONTAINERNAME" == "" ]
+then
+	exit 1
+fi
+
+# Step 2 : Check if the attribute is in the docker columns list and find its next column
 HAS_COLUMN=0
 NEXT_COLUMN=""
 DOCKER_COLUMNS=("CONTAINER ID" "IMAGE" "COMMAND" "CREATED" "STATUS" "PORTS" "NAMES")
@@ -39,27 +45,33 @@ do
 	if [ "${DOCKER_COLUMNS[$COLUMN_IDX]}" == "$ATTRIBUTE" ]
 	then
 		HAS_COLUMN=1
-		NEXT_COLUMN=${DOCKER_COLUMNS[$COLUMN_IDX+1]}
+		if [ "$COLUMN_IDX" != "#DOCKER_COLUMNS[@]" ]
+		then
+			NEXT_COLUMN=${DOCKER_COLUMNS[$COLUMN_IDX+1]}
+		fi
 	fi
 done
-
 if [ "$HAS_COLUMN" != "1" ]
 then
-	exit 1
+	exit 2
 fi
 
-# Step 2 : Check of it exists a container with the given name
+# Step 3 : Check of it exists a container with the given name
 DOCKER_PS=$(docker ps -a 2>/dev/null)
 DOCKER_INFO=$(echo "$DOCKER_PS" | awk -v v_CONTAINERNAME="$CONTAINERNAME" -F' ' '{if($NF==v_CONTAINERNAME){print $0}}' | tail -n1)
 if [ "$DOCKER_INFO" == "" ]
 then
-	exit 1
+	exit 3
 fi
 
 DOCKER_HEADER=$(echo "$DOCKER_PS" | head -n1)
 
 COLUMN_POS_BEGIN=$(find_word_position "$ATTRIBUTE"   "$DOCKER_HEADER")
-COLUMN_POS_END=$(find_word_position   "$NEXT_COLUMN" "$DOCKER_HEADER")
+if [ "$NEXT_COLUMN" != "" ]
+then
+	COLUMN_POS_END=$(find_word_position   "$NEXT_COLUMN" "$DOCKER_HEADER")
+	COLUMN_POS_END=$((COLUMN_POS_END-1))
+fi
 
 echo "$DOCKER_INFO" | cut -c"$COLUMN_POS_BEGIN"-"$COLUMN_POS_END"
 exit 0
